0b13c090220b56da24d455e021c21afe4340b634
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-03-13 17:49:10 +0530

    feat: adds express.js with bun to `package.json`

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Bun + Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
