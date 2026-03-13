3bc00fb12f25bf604254f1cb5f3011aacf07f907
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-03-13 23:40:54 +0530

    feat: implement basic bcrypt logic in hashing password and add register function as a promise type

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import appconfig from "../dotenv";
+import * as bcrypt from "bcrypt";
 
 const adapter = new PrismaPg({
   connectionString: appconfig.DATABASE_URL,
@@ -10,24 +11,55 @@ const adapter = new PrismaPg({
 const prisma = new PrismaClient({ adapter: adapter });
 
 export const login = async (_req: Request, res: Response) => {
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-                username: 'Aryan-202'
-            }
-        })
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: "Aryan-202",
+      },
+    });
 
-        if (!user) {
-            return res.status(404).json({
-                msg: 'available nahi hai user'
-            })
-        }
-        return res.status(200).json({
-            msg: 'available hai user'
-        })
-    } catch (error) {
-        res.status(500).json({
-            msg: error
-        })
+    if (!user) {
+      return res.status(404).json({
+        msg: "available nahi hai user",
+      });
     }
-}
+    return res.status(200).json({
+      msg: "available hai user",
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: error,
+    });
+  }
+};
+
+export const register = async (req: Request, res: Response): Promise<any> => {
+  const { email, name, username, password, confirmPassword } = req.body;
+
+  if (!username || !name || !email || !password || !confirmPassword) {
+    res.status(404).json({
+      msg: "dekh kuch miss ho ra",
+    });
+  }
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ email: email }, { username: username }],
+    },
+  });
+
+  if (existingUser) {
+    return res.status(409).json({
+      msg: "bhai tu already exist karta hai",
+    });
+  }
+
+  const saltRounds = 10;
+  if (password === confirmPassword) {
+    const hashpassword = bcrypt.hash(password, saltRounds);
+  } else {
+    return res.json({
+      msg: "password match nhi kar ra",
+    });
+  }
+};
