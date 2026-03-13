d9bb337425c06f6baecfd8281606a8268cc81001
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-03-13 20:12:27 +0530

    feat: implement prisma with seed data

--- server/dotenv.ts
+++ server/dotenv.ts
@@ -1,6 +1,7 @@
 const appconfig = {
     PORT: process.env.PORT || 3000,
     NODE_ENV:  process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL!
 }
 
 export default appconfig
---
+++ server/prisma/seed.ts
@@ -0,0 +1,23 @@
+import appconfig from "../dotenv";
+import { PrismaClient, Prisma } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: appconfig.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter: adapter,
+});
+
+const userData: Prisma.UserCreateInput[] = [
+  { email: "aryan@gmail.com", name: "Aryan", username: "Aryan-202" },
+];
+
+export async function main() {
+    for(const u of userData) {
+        await prisma.user.create({
+            data: u
+        })
+    }
+}
