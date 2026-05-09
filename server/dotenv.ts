5ab5bf39638bc151153504098c71988658e7b1a9
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-05-10 01:49:48 +0530

    feat: initialize Tauri application with Leptos integration
    - Added main application logic in `src-tauri/src/lib.rs` and `src-tauri/src/main.rs`
    - Created Tauri configuration in `src-tauri/tauri.conf.json`
    - Implemented greeting functionality with Rust command in Tauri
    - Developed frontend application structure in `src/app.rs` using Leptos
    - Set up main entry point in `src/main.rs` to mount the Leptos app
    - Added basic styling in `styles.css` for a better user interface

--- server/dotenv.ts
+++
@@ -1,8 +0,0 @@
-const appconfig = {
-    PORT: process.env.PORT || 3000,
-    NODE_ENV:  process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL!,
-    JWT_SECRET: process.env.JWT_SECRET!
-}
-
-export default appconfig
